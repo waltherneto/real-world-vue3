88eaf61df96e3390691389387ae37b58f83a7b73
root <walther@plannmarketing.com.br>
2021-04-03 19:07:56 +0000

    Aplicação EventCard

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  baseURL: 'https://my-json-server.typicode.com/waltherneto/jsonserver',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
   getEvents() {
     return apiClient.get('/events')
+  },
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
   }
 }
